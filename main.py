e1a329a0f4b88cc04d1c4f697ef1239ff345a508
Duy Nguyen <ngdangduy2009@gmail.com>
2026-03-26 21:35:56 +1100

    feat: Configure ClearML by setting its config file path in `main.py` and ignoring it in `.gitignore`.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 
+# Point ClearML to the local config file before importing it
+os.environ["CLEARML_CONFIG_FILE"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "clearml.conf")
+
 from sync_dataset import upload_dataset
 
 path = "./data/"
